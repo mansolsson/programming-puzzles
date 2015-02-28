7bdc86fe1a8e75e968200506570fa465d7d66584
Måns Olsson <mans2111@gmail.com>
2015-02-28 17:36:26 +0100

    Added solution for problem 9

---
+++ problem009/main.py
@@ -0,0 +1,13 @@
+import math
+
+
+def findAnswer():
+    for a in range(1, 333):
+        for b in range(a + 1, math.floor((1000 - a) / 2)):
+            c = 1000 - a - b
+            if a ** 2 + b ** 2 == c ** 2:
+                return a * b * c
+    return 0
+
+if __name__ == "__main__":
+    print(findAnswer())
