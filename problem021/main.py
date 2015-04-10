efb02c1759e6340f42a1e00f831ef393efbe2396
Måns Olsson <mans2111@gmail.com>
2015-04-10 21:20:52 +0200

    Added solution for problem 21

---
+++ problem021/main.py
@@ -0,0 +1,31 @@
+def getSumOfDivisors(number):
+    divisors = []
+    if number % 2 == 0:
+        inc = 1
+    else:
+        inc = 2
+
+    stop = number // 2
+    i = 1
+    while i < stop:
+        if number % i == 0:
+            stop = number // i
+            if stop == i:
+                divisors.append(stop)
+            else:
+                divisors.append(stop)
+                divisors.append(i)
+        i += inc
+
+    return sum(divisors) - number
+
+if __name__ == "__main__":
+    sumOfDivisors = [0, 0]
+    for i in range(2, 10001):
+        sumOfDivisors.append(getSumOfDivisors(i))
+
+    sumOfAmicableNrs = 0
+    for i in range(2, 10001):
+        if sumOfDivisors[i] < 10000 and sumOfDivisors[sumOfDivisors[i]] == i and sumOfDivisors[i] != i:
+            sumOfAmicableNrs += sumOfDivisors[i]
+    print(sumOfAmicableNrs)
